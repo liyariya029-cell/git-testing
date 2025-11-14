1c91e1edb8ab038d597d688a21e5bf21877f9a47
liyariya029-cell <liyariya029@gmail.com>
2025-11-14 11:54:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PhoneBook from './components/PhoneBook';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<PhoneBook />} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
